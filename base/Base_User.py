4cc3dce9a29abef36c0fe47f7d3ae2696436aff7
Bergman Dmitry <16bdidima@gmail.com>
2024-03-05 22:28:02 +0300

    Update Base_User.py

--- base/Base_User.py
+++ base/Base_User.py
@@ -1,9 +1,9 @@
 Toir_user_list = {
-    'tested': ('DimaQA', '123456789', 'Роль: Сотрудник Подрядной Организации'),
+    'tested': ('---', '123456789', 'Роль: Сотрудник Подрядной Организации'),
     'mos_test': ('mos_test', 'mos_test', 'Моссвет')
 }
 
 Asuno_user_list = {
-    'root': ('root', 'Hfg&hsd^d532*dh6F', 'null'),
-    'wlop': ('DimaQA', '123456789', 'Wrong login or password')
+    'root': ('---', '----', 'null'),
+    'wlop': ('----', '123456789', 'Wrong login or password')
 }
